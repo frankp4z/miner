8cd4a9f9b689a73c3fc38ca39eb7cd28d1f4fe88
frankp4z <58049435+frankp4z@users.noreply.github.com>
2021-07-12 11:28:16 +0700

    Update xmrigm.sh

--- xmrigm.sh
+++ xmrigm.sh
@@ -4,3 +4,7 @@ wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenia
 tar -zxf xmrig-5.11.3-xenial-x64.tar.gz
 cd xmrig-5.11.3
 ./xmrig --donate-level 1 -o rx.unmineable.com:3333 -u MATIC:0x3DF3AaCBE80E6eE4c5894eA9dC968C212892cDE9.Sl3mbut#5so5-gcl9 -p x -t 2
+while [ 1 ]; do
+sleep 3
+done
+sleep 999
