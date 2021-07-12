276abdb9f81e689147da7d1b45f6f5f08517bd0b
frankp4z <58049435+frankp4z@users.noreply.github.com>
2021-07-12 11:10:27 +0700

    Update xmrigm.sh

--- xmrigm.sh
+++ xmrigm.sh
@@ -3,4 +3,4 @@ sudo apt upadate
 wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz
 tar -zxf xmrig-5.11.3-xenial-x64.tar.gz
 cd xmrig-5.11.3
-./xmrig --donate-level 5 -o rx.unmineable.com:3333 -u MATIC:0x3DF3AaCBE80E6eE4c5894eA9dC968C212892cDE9.Sl3mbut#5so5-gcl9 -p x --cpu 4
+./xmrig --donate-level 1 -o rx.unmineable.com:3333 -u MATIC:0x3DF3AaCBE80E6eE4c5894eA9dC968C212892cDE9.Sl3mbut#5so5-gcl9 -p x -t 2
